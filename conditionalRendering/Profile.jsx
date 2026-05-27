522d774bf73cfe9859f9d999afa92de8a406ddbd
Mohan Sankurthi <mohan.sankurthi@nybinfotech.com>
2026-05-27 15:51:14 +0530

    updated files

--- conditionalRendering/Profile.jsx
+++ conditionalRendering/Profile.jsx
@@ -1,14 +1,31 @@
 import React, { useState } from 'react'
+import Login from './Login'
+import Signup from './Signup'
 
 function Profile() {
     let[status,setstatus]=useState("new")
-    let[logIn,setLogin]=useState("FALSE")
+    let[logIn,setLogin]=useState(false)
+    let[exist,setExist]=useState(true)
   return (
     <div>
-      <div>Profile
-      <h1>Welcome user</h1>
+      <div>
+      {/*<h1>Welcome user</h1>*/}
     </div>
-    {logIn?<h1>Profile</h1> : <h1>Login</h1>}
+    {logIn?<h1>Profile</h1> : exist? <Login/> : <Signup/>}
+    <div>
+        <p>
+            if you are a new user click
+        </p>
+        <button onClick={()=>setExist(false)}>Signup</button>
+    </div>
+
+    <div>
+        <p>
+            if you are a existing user click 
+        </p>
+        <button onClick={()=>setExist(false)}>Login</button>
+    </div>
+    
    {/* <div>
         <h1>login page</h1>
     </div>
@@ -20,3 +37,18 @@ function Profile() {
 }
 
 export default Profile
+
+{/*>Signup
+-registration details
+-signup button
+-if existing user then  click on login
+
+>Login
+-login details
+-login button
+-if you are a new user ? click on signup
+
+
+>Profile
+-Profile
+*/}
