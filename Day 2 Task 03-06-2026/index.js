85ecfd2a298551851c5b5bb2c446937adfa5d3a1
Mohan Sankurthi <mohan.sankurthi@nybinfotech.com>
2026-06-03 14:43:40 +0530

    dailytasks

---
+++ Day 2 Task 03-06-2026/index.js
@@ -0,0 +1,29 @@
+console.log("this is js file")
+var a=10
+var abc=`30`
+var name="mohan"
+var bool=true
+var k
+var m=null
+
+console.log(a)
+console.log(abc)
+console.log(name)
+console.log(bool)
+console.log(k)
+console.log(m)
+
+//typeOf operator
+console.log(typeof(a))
+console.log(typeof(abc))
+console.log(typeof(name))
+console.log(typeof(bool))
+console.log(typeof(k))
+console.log(typeof(m))
+
+
+/*variables -> to store the data
+2 types of datatypes
+1.Primitive datatype-> stores the single value in single memory location
+2.Reference or non-primitive datatype->multiple values in a single memory location */
+
